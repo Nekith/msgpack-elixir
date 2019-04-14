4314cf3e6ca36db2177be0719119a7e41a8e0131
Nekith <nekith@errant-works.com>
2019-04-14 18:34:30 +0200

    fix compile error

--- lib/message_pack/ext.ex
+++ lib/message_pack/ext.ex
@@ -2,8 +2,8 @@ defmodule MessagePack.Ext do
 
   @type type :: non_neg_integer
 
-  defcallback pack(term) :: { :ok, { type, binary } } | { :error, term }
-  defcallback unpack(type, binary) :: { :ok, term } | { :error, term }
+  @callback pack(term) :: { :ok, { type, binary } } | { :error, term }
+  @callback unpack(type, binary) :: { :ok, term } | { :error, term }
 
   defmodule Behaviour do
     defmacro __using__(_) do
